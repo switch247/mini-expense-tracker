7a1b59e7ab699c8c25983b7ad0d61af1350fd175
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-12-27 14:54:34 +0300

    update database connection string to use env variables

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -5,6 +5,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DB_CONNECTION_STRING);
+const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
